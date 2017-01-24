e28038f24b7e8284f411588b5002cf08bfcbe3bf
Charlie <gcs@iastate.edu>
2017-01-24 09:42:15 -0600

    Initial Commit

---
+++ XNA_tutorial_1/XNA_tutorial_1/Game1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using Microsoft.Xna.Framework;
+ using Microsoft.Xna.Framework.Audio;
+ using Microsoft.Xna.Framework.Content;
+ //using Microsoft.Xna.Framework.GamerServices;
+ using Microsoft.Xna.Framework.Graphics;
+ using Microsoft.Xna.Framework.Input;
+ using Microsoft.Xna.Framework.Media;
+ 
+ namespace XNA_tutorial_1
+ {
+     public class Game1 : Microsoft.Xna.Framework.Game
+     {
+         GraphicsDeviceManager graphics;
+         SpriteBatch spriteBatch;
+         GraphicsDevice device;
+         Effect effect;
+ 
+         Matrix viewMatrix;
+         Matrix projectionMatrix;
+ 
+         public Game1()
+         {
+             graphics = new GraphicsDeviceManager(this);
+             Content.RootDirectory = "Content";
+         }
+ 
+         protected override void Initialize()
+         {
+             graphics.PreferredBackBufferWidth = 500;
+             graphics.PreferredBackBufferHeight = 500;
+             graphics.IsFullScreen = false;
+             graphics.ApplyChanges();
+             Window.Title = "Riemer's XNA Tutorials -- 3D Series 2";
+ 
+             base.Initialize();
+         }
+ 
+         protected override void LoadContent()
+         {
+             spriteBatch = new SpriteBatch(GraphicsDevice);
+             
+             device = graphics.GraphicsDevice;
+
+            effect = Content.Load<Effect> ("effects");           
+            SetUpCamera();
+        }
+
+        private void SetUpCamera()
+        {
+            viewMatrix = Matrix.CreateLookAt(new Vector3(0, 0, 30), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, device.Viewport.AspectRatio, 0.2f, 500.0f);
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            device.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.DarkSlateBlue, 1.0f, 0);
+
+            base.Draw(gameTime);
+        }
+    }
+}
